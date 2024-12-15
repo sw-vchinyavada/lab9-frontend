c2b8ee82d91b2aabb9b70bbf2062204739ea8d56
Victor Chinyavada <victor.chinyavada@momentum.co.za>
2024-12-15 12:30:11 +0200

    added components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@fontsource/poppins';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+import './styles/GlobalStyles.css';
+import { store } from './app/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
